a48e72273d99b737b3d72fc8ef2b9f3f57090629
Jim Daehn <jdaehn@missouristate.edu>
2017-05-06 19:44:32 -0500

    CSC232-HW01 - Initial import of src tree.

---
+++ src/main/Demo.cpp
@@ -0,0 +1,19 @@
+/**
+ * CSC232 Data Structures with C++
+ * Missouri State University, Fall 2017
+ *
+ * @file Demo.cpp
+ * @authors Jim Daehn <jdaehn@missouristate.edu>
+ *          Your Name <yname@missouristate.edu>
+ *          Partner Name <pname@missouristate.edu>
+ * @brief   Entry point for demo application.
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char **argv) {
+    std::cout << "Hello, Demo" << std::endl;
+
+    return EXIT_SUCCESS;
+}
