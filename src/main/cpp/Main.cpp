39d58ece87958f94ee1a30fe240bcf0c709110ce
Mr. Daehn <jdaehn@missouristate.edu>
2017-09-06 02:03:30 -0500

    CSC232-HW01 - Update header comments.

--- src/main/cpp/Main.cpp
+++ src/main/cpp/Main.cpp
@@ -5,7 +5,6 @@
  * @file Main.cpp
  * @authors Jim Daehn <jdaehn@missouristate.edu>
  *          Your Name <yname@missouristate.edu>
- *          Partner Name <pname@missouristate.edu>
  * @brief   Entry point for this application. This is nothing more than a playground.
  */
 
